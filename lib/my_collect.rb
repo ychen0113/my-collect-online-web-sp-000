6d0a69614f1d276aad816f944053f566f3d1dbf9
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-22 20:24:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.length
-  collection << yield(array[i])
+  collection << yield(array[i]) if block_given?
   i += 1
   end
   collection
